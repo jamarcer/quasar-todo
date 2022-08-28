dc6ed5004cea5dbe543a4a3a80a1c71babc2cc30
Jesus Martinez <jamartinez@undanet.com>
2022-08-28 17:37:24 +0200

    Added tasks functionalities: + Add new tasks + Check a tas as done + Delete task

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,12 +1,49 @@
 <template>
   <q-page class="bg-grey-3 column">
+    <div class="row q-pa-sm bg_primary">
+      <q-input
+        v-model="newTask"
+        @keyup.enter="addTask"
+        class="col"
+        square
+        filled
+        bg-color="white"
+        placeholder="Add task"
+        dense
+      >
+        <template v-slot:append>
+          <q-btn @click="addTask" round dense flat icon="add" />
+        </template>
+      </q-input>
+    </div>
     <q-list class="bg-white" separator bordered>
-      <q-item v-for="task in tasks" :key="task.title" v-ripple>
+      <q-item
+        v-for="(task, index) in tasks"
+        :key="task.title"
+        @click="task.done = !task.done"
+        :class="{ 'done bg-blue-1': task.done }"
+        clickable
+        v-ripple
+      >
         <q-item-section avatar>
-          <q-checkbox v-model="task.done" color="primary" />
+          <q-checkbox
+            v-model="task.done"
+            class="no-pointer-events"
+            color="primary"
+          />
         </q-item-section>
         <q-item-section>
           <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+        <q-item-section v-if="task.done" side>
+          <q-btn
+            @click.stop="deleteTask(index)"
+            flat
+            round
+            dense
+            color="primary"
+            icon="delete"
+          />
         </q-item-section>
       </q-item>
     </q-list>
@@ -19,21 +56,47 @@ import { defineComponent } from "vue";
 export default defineComponent({
   data() {
     return {
-      tasks: [
-        {
-          title: "Buy bananas",
-          done: false,
-        },
-        {
-          title: "Eat bananas",
-          done: true,
-        },
-        {
-          title: "Check bananas",
-          done: false,
-        },
-      ],
+      newTask: "",
+      tasks: [],
     };
+  },
+  methods: {
+    deleteTask(index) {
+      this.$q
+        .dialog({
+          title: "Prompt",
+          message: "Really delete?",
+          prompt: {
+            model: "",
+            type: "text", // optional
+          },
+          cancel: true,
+          persistent: true,
+        })
+        .onOk((data) => {
+          this.tasks.splice(index, 1);
+          this.$q.notify("Task deleted");
+        })
+        .onCancel(() => {
+          console.log(">>>> Cancel");
+        });
+    },
+    addTask() {
+      this.tasks.push({
+        title: this.newTask,
+        done: false,
+      });
+      this.newTask = "";
+    },
   },
 });
 </script>
+
+<style lang="scss">
+.done {
+  .q-item__label {
+    text-decoration: line-through;
+    color: #bbb;
+  }
+}
+</style>
