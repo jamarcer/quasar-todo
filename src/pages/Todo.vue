8e437c9fea995269bf172858f4d319c9d1c84850
Jesus Martinez <jamartinez@undanet.com>
2022-08-28 16:32:51 +0200

    Added show tasks functionality

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -1,11 +1,39 @@
 <template>
-  <q-page>
-    <h5>Todo</h5>
+  <q-page class="bg-grey-3 column">
+    <q-list class="bg-white" separator bordered>
+      <q-item v-for="task in tasks" :key="task.title" v-ripple>
+        <q-item-section avatar>
+          <q-checkbox v-model="task.done" color="primary" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 
-export default defineComponent({});
+export default defineComponent({
+  data() {
+    return {
+      tasks: [
+        {
+          title: "Buy bananas",
+          done: false,
+        },
+        {
+          title: "Eat bananas",
+          done: true,
+        },
+        {
+          title: "Check bananas",
+          done: false,
+        },
+      ],
+    };
+  },
+});
 </script>
